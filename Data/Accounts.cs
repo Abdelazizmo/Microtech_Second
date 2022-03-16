c811951c259681cda2e7490225f557ceada9d456
MZ <abdelazizmohamed985@gmail.com>
2022-03-16 21:22:01 +0200

    first commit

---
+++ Data/Accounts.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Microtech_WebApiTask_3.Data
+{
+    public class Accounts
+    {
+        public Accounts() => Children = new HashSet<Accounts>();
+
+        public string Id { get; set; }
+        public string Parent { get; set; }
+        public decimal? Balance { get; set; }
+        public decimal Total { get; set; }
+        public ICollection<Accounts> Children { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microtech_WebApiTask_3.Data;
+
+namespace Microtech_WebApiTask_3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddScoped<ControlData>();
+            services.AddControllers();
+        }
+
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
